43558f4eb3a2876ab18adaaa25e4cd509a1c145b
joseRmoura <jrmoura.dev@gmail.com>
2020-11-05 13:54:20 -0300

    api

--- src/main/java/com/banco/emprestimo/repository/EmprestimoRepository.java
+++ src/main/java/com/banco/emprestimo/repository/EmprestimoRepository.java
@@ -1,6 +1,7 @@
 package com.banco.emprestimo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.banco.emprestimo.model.Emprestimo;
+import com.banco.emprestimo.model.Usuario;
+
 
 @Repository
 public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long> {
